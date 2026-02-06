e2f4cf6319a37a6cd9b314c68c04c9a0d0ce1dd7
zcorw <zhzhwwt@gmail.com>
2026-02-06 22:22:38 +0900

    feat: 退出账号

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
-import { Button } from '@mui/material';
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Layout from './components/Layout';
 import SiteHeader from './components/SiteHeader';
 import AppRoutes from './routes';
 import GlobalSnackbar from './components/GlobalSnackbar';
+import useAuthStore from './store/auth';
 
 function App() {
+  const [accountMenuAnchor, setAccountMenuAnchor] =
+    useState<null | HTMLElement>(null);
+  const { clearSession } = useAuthStore();
+  const accountMenuOpen = Boolean(accountMenuAnchor);
+
+  const handleAccountMenuOpen = (
+    event: React.MouseEvent<HTMLElement>
+  ) => {
+    setAccountMenuAnchor(event.currentTarget);
+  };
+
+  const handleAccountMenuClose = () => {
+    setAccountMenuAnchor(null);
+  };
+
+  const handleLogout = () => {
+    clearSession();
+    handleAccountMenuClose();
+  };
+
   return (
     <>
       <SiteHeader
         actions={
-          <Button variant="contained" color="primary" component={Link} to="/expenses">
-            快速记一笔
-          </Button>
+          <>
+            <IconButton
+              size='large'
+              onClick={handleAccountMenuOpen}
+              aria-controls={accountMenuOpen ? 'account-menu' : undefined}
+              aria-haspopup='true'
+              aria-expanded={accountMenuOpen ? 'true' : undefined}
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id='account-menu'
+              anchorEl={accountMenuAnchor}
+              open={accountMenuOpen}
+              onClose={handleAccountMenuClose}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              keepMounted
+            >
+              <MenuItem onClick={handleLogout}>退出账号</MenuItem>
+            </Menu>
+          </>
         }
       />
       <Layout>
